fb0a27309e3601adb51528af4aca7f0b37fc34ec
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-15 08:55:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,40 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+const devs = ["466425075487342615"];
+const adminprefix = ["h"];
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
 
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "lev")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'w')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'l')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
+  }
+  if (message.content.startsWith(adminprefix + 'name')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
+}
+});//hamo
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
